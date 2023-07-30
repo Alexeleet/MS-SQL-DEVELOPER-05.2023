122fc33ba130ad65750c50adfbae4f46c2ab68a7
Alexeleet <50372978+Alexeleet@users.noreply.github.com>
2023-07-30 11:12:45 +0500

    Add files via upload

---
+++ ДЗ 9/hw_dynamic_sql_tasks-188-19bdb8.sql
@@ -0,0 +1,62 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "07 - Динамический SQL".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+
+Это задание из занятия "Операторы CROSS APPLY, PIVOT, UNPIVOT."
+Нужно для него написать динамический PIVOT, отображающий результаты по всем клиентам.
+Имя клиента указывать полностью из поля CustomerName.
+
+Требуется написать запрос, который в результате своего выполнения 
+формирует сводку по количеству покупок в разрезе клиентов и месяцев.
+В строках должны быть месяцы (дата начала месяца), в столбцах - клиенты.
+
+Дата должна иметь формат dd.mm.yyyy, например, 25.12.2019.
+
+Пример, как должны выглядеть результаты:
+-------------+--------------------+--------------------+----------------+----------------------
+InvoiceMonth | Aakriti Byrraju    | Abel Spirlea       | Abel Tatarescu | ... (другие клиенты)
+-------------+--------------------+--------------------+----------------+----------------------
+01.01.2013   |      3             |        1           |      4         | ...
+01.02.2013   |      7             |        3           |      4         | ...
+-------------+--------------------+--------------------+----------------+----------------------
+*/
+
+
+USE WideWorldImporters;
+
+DECLARE @dml AS NVARCHAR(MAX)
+DECLARE @CustomerName as NVARCHAR(MAX)
+SELECT @CustomerName = ISNULL(@CustomerName +', ', '') + '[' + CustName + ']'
+FROM (SELECT DISTINCT (CustomerName) as CustName FROM Sales.Customers) AS q1
+
+SET @dml = N'
+WITH CountSalesCTE AS (
+	SELECT FORMAT(CAST(STR(10000 * Year(invoiceDate) + 100 * Month(invoiceDate) + 1) AS date), ''d'', ''de-de'') AS Дата
+		, CustomerName 
+	FROM Sales.Invoices JOIN Sales.Customers ON Invoices.CustomerID = Customers.CustomerID)
+SELECT * FROM CountSalesCTE
+PIVOT (count(CustomerName) FOR CustomerName IN (' + @CustomerName + ')) AS pivotTable'
+
+EXEC (@dml)
+
+
